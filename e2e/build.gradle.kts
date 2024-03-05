3cdb056a2e249b381f90c048cb939a29ab1a827a
Irene Wang <lohas1107@gmail.com>
2024-03-05 10:49:49 +0800

    build: add test gradle task

--- e2e/build.gradle.kts
+++ e2e/build.gradle.kts
@@ -4,3 +4,24 @@
  * This is a general purpose Gradle build.
  * To learn more about Gradle by exploring our Samples at https://docs.gradle.org/8.6/samples
  */
+
+plugins {
+    java
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
