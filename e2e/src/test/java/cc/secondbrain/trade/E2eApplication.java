b52159ff1bb3914b02f00c2da98248ce5052ea9f
Irene Wang <lohas1107@gmail.com>
2024-03-16 20:54:51 +0800

    refactor: rename to e2e application

--- e2e/src/test/java/cc/secondbrain/trade/E2eApplication.java
+++ e2e/src/test/java/cc/secondbrain/trade/E2eApplication.java
@@ -1,9 +1,13 @@
 package cc.secondbrain.trade;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
-public class TradeApplication {
+public class E2eApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(E2eApplication.class, args);
+    }
 }
