d3df98259229920985fba394961f78b9992c617c
Irene Wang <lohas1107@gmail.com>
2024-03-18 09:28:33 +0800

    feat: create place order endpoint

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let place_order = warp::post()
+        .and(warp::path("v1"))
+        .and(warp::path("stack"))
+        .and(warp::path("orders"))
+        .map(warp::reply);
+
+    let route = place_order;
+
+    warp::serve(route)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
 }
--- e2e/src/test/resources/api/order.feature
+++ e2e/src/test/resources/api/order.feature
@@ -10,3 +10,12 @@ Feature: 下單
     Given exists data "轉帳紀錄":
       | deliveryAccount.accountNumber | amount | balance |
       | 00123456789                   | 100000 | 100000  |
+    When POST "/v1/stack/orders":
+    """
+    {
+      "action": "BUY",
+      "code": "2890",
+      "price": 20,
+      "share": 1000,
+    }
+    """
