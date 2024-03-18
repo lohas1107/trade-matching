d754f865e7bf55b7290469ca57d668e5a205e536
Irene Wang <lohas1107@gmail.com>
2024-03-18 10:34:25 +0800

    refactor: extract module route

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,18 +1,8 @@
-use warp::{Filter, Rejection};
+mod route;
 
 #[tokio::main]
 async fn main() {
-    warp::serve(routes())
+    warp::serve(route::route())
         .run(([127, 0, 0, 1], 3030))
         .await;
-}
-
-fn routes() -> impl Filter<Extract=(impl warp::Reply, ), Error=Rejection> + Clone {
-    let place_order = warp::post()
-        .and(warp::path("v1"))
-        .and(warp::path("stack"))
-        .and(warp::path("orders"))
-        .map(warp::reply);
-
-    place_order
 }
---
+++ backend/src/route.rs
@@ -0,0 +1,11 @@
+use warp::{Filter, Rejection};
+
+pub fn route() -> impl Filter<Extract=(impl warp::Reply, ), Error=Rejection> + Clone {
+    let place_order = warp::post()
+        .and(warp::path("v1"))
+        .and(warp::path("stack"))
+        .and(warp::path("orders"))
+        .map(warp::reply);
+
+    place_order
+}
