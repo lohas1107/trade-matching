4d2a9f6bf19b12923234ca160bc5322c31fa1dd8
Irene Wang <lohas1107@gmail.com>
2024-03-21 09:21:58 +0800

    refactor: extract filter and handler functions

--- backend/src/route.rs
+++ backend/src/route.rs
@@ -1,13 +1,21 @@
 use warp::{Filter, path, Rejection, Reply};
+use warp::filters::BoxedFilter;
+use warp::http::StatusCode;
 
-fn order_path() -> impl Filter<Extract = (), Error = Rejection> + Copy {
+fn order_path() -> impl Filter<Extract=(), Error=Rejection> + Copy {
     path!("v1" / "stack" / "orders")
 }
 
 pub fn route() -> impl Filter<Extract=(impl Reply, ), Error=Rejection> + Clone {
-    let place_order = warp::post()
-        .and(order_path())
-        .map(warp::reply);
+    post_order().map(create_order())
+}
 
-    place_order
+pub fn post_order() -> BoxedFilter<()> {
+    warp::post()
+        .and(order_path())
+        .boxed()
+}
+
+pub fn create_order() -> fn() -> StatusCode {
+    || { StatusCode::CREATED }
 }
