3b66fb5b8f37afb7c035837077e540171ce9bb9d
Irene Wang <lohas1107@gmail.com>
2024-03-26 09:39:53 +0800

    wip

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,6 +2,22 @@ mod route;
 mod order;
 mod migration;
 
+
+// embed_migrations!("../migrations");
+// 
+// fn main() {
+//     let connection = establish_connection();
+// 
+//     // This will run the necessary migrations.
+//     embedded_migrations::run(&connection);
+// 
+//     // By default the output is thrown out. If you want to redirect it to stdout, you
+//     // should call embedded_migrations::run_with_output.
+//     embedded_migrations::run_with_output(&connection, &mut std::io::stdout());
+// }
+
+
+
 #[tokio::main]
 async fn main() {
     warp::serve(route::route())
--- backend/src/migration.rs
+++ backend/src/migration.rs
@@ -1,6 +1,7 @@
+use std::error::Error;
 use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
-pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("../../migrations/postgresql");
-
+pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
+type DB=diesel::mysql::Mysql;
 
 fn run_migrations(connection: &mut impl MigrationHarness<DB>) -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
     // https://docs.rs/diesel_migrations/latest/diesel_migrations/macro.embed_migrations.html
@@ -12,3 +13,17 @@ fn run_migrations(connection: &mut impl MigrationHarness<DB>) -> Result<(), Box<
 
     Ok(())
 }
+
+
+// embed_migrations!("./mysql");
+//
+// fn main() {
+//     let connection = establish_connection();
+//
+//     // This will run the necessary migrations.
+//     embedded_migrations::run(&connection);
+//
+//     // By default the output is thrown out. If you want to redirect it to stdout, you
+//     // should call embedded_migrations::run_with_output.
+//     embedded_migrations::run_with_output(&connection, &mut std::io::stdout());
+// }
