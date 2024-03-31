d3833c6eb4a2d0c23d2cd76c9b5f843c69c975e7
Irene Wang <lohas1107@gmail.com>
2024-03-31 15:10:51 +0800

    feat: print log

--- backend/stock/src/main.rs
+++ backend/stock/src/main.rs
@@ -1,8 +1,15 @@
+use std::env;
+
 mod route;
 mod order;
 
 #[tokio::main]
 async fn main() {
+    if env::var_os("RUST_LOG").is_none() {
+        env::set_var("RUST_LOG", "debug");
+    }
+    pretty_env_logger::init();
+    
     warp::serve(route::route())
         .run(([127, 0, 0, 1], 3030))
         .await;
