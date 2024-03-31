9bd10c47cd60e6ba1535b1e975632c400b63de73
Irene Wang <lohas1107@gmail.com>
2024-03-31 15:15:35 +0800

    feat: connect to database

--- backend/stock/src/order/handler.rs
+++ backend/stock/src/order/handler.rs
@@ -1,5 +1,11 @@
+use std::convert::Infallible;
+use diesel::MysqlConnection;
+
 use warp::http::StatusCode;
 
-pub fn create_order() -> fn() -> StatusCode {
-    || { StatusCode::CREATED }
+use crate::route::Pool;
+
+pub async fn create_order(pool: Pool) -> Result<impl warp::Reply, Infallible> {
+    let _: &MysqlConnection = &pool.get().unwrap();
+    Ok(StatusCode::CREATED)
 }
--- backend/stock/src/route.rs
+++ backend/stock/src/route.rs
@@ -1,6 +1,25 @@
-use warp::{Filter, Rejection, Reply};
+use std::error::Error;
+
+use diesel::{MysqlConnection, r2d2};
+use diesel::r2d2::ConnectionManager;
+use warp::Filter;
+use warp::filters::BoxedFilter;
+
 use crate::order::{handler, path};
 
-pub fn route() -> impl Filter<Extract=(impl Reply, ), Error=Rejection> + Clone {
-    path::post_order().map(handler::create_order())
+pub fn route() -> impl Filter<Extract=(impl warp::Reply, ), Error=warp::Rejection> + Clone {
+    let database = database().expect("Database");
+
+    path::post_order()
+        .and(database.clone())
+        .and_then(handler::create_order)
+}
+
+pub type Pool = r2d2::Pool<ConnectionManager<MysqlConnection>>;
+
+pub fn database() -> Result<BoxedFilter<(Pool, )>, Box<dyn Error>> {
+    let url = "mysql://root:root@host.docker.internal:3306/trade";
+    let manager = ConnectionManager::<MysqlConnection>::new(url);
+    let pool = Pool::builder().build(manager)?;
+    Ok(warp::any().map(move || pool.clone()).boxed())
 }
